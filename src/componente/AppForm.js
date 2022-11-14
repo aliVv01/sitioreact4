cc498c2948b569ee33c886679210f188fcc01fd4
aliVv01 <alivvelarde99@gamil.com>
2022-11-13 21:56:01 -0500

    V04 my-app1(Main).Se tiene la llamada a AppForm.js en App.js

--- src/componente/AppForm.js
+++ src/componente/AppForm.js
@@ -1,8 +1,10 @@
 import {collection, doc, getDoc, addDoc, updateDoc} from "firebase/firestore";
 //import {getDocs, query, setDoc, where, deleteDoc} from "firebase/firestore";
-import React from 'react';
+import { useState } from "react";
 import firebase, { db } from './firebase';
 import { async } from '@firebase/util';
+
+
 
 const AppForm = (props) => {
     ///////////////////////////////////////////////////////////////
@@ -10,8 +12,8 @@ const AppForm = (props) => {
     ///////////////////////////////////////////////////////////////
     const camposRegistro = {nombre:"", edad:"", genero:""};   //estructura tbL
     const [objeto, setObjeto] = useState(camposRegistro);     //Tabla u objeto
-
-    const handleStatuschange = (e) => { 
+     //////////////////////   UPDATE fnUpdate   //////////////////////
+    const handleStatusChange = (e) => { 
         const {name, value} = e.target;
         setObjeto({...objeto, [name]:value });
         console.log(objeto);
@@ -22,17 +24,19 @@ const AppForm = (props) => {
     const validarForm =  (e) => { 
         console.log("Maneja envio");
     };                                    //Manejo cambios en input...
-       
-     return (
+
+    return (
         <div style={{background:"yellow", padding:"10px", margin:"10px"}}>  
            <h1>AppForm.js</h1>
            <form onSubmit = {handleSubmit}>
                <input type ="text" name="nombre" placeholder="escriba completo..." 
-               onChange ={handleStatusChange} value ={obejti.nombre}/> <br/>
+               onChange ={handleStatusChange} value ={objeto.nombre}/> <br/>
                <button>
-                  {props.idActual} === ""? "Guardar" : "Actualizar" ----
+                  {props.idActual  === ""? "Guardar" : "Actualizar"}
                </button>       
             </form>
         </div>           //Ver en tiempo real
-     )
+    )
 }
+
+export default AppForm
